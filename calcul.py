6ac5fd9de954996d4a1966a4fbf4d065e4f12cd7
bennayya <ayabennouiri123@gmail.com>
2025-04-27 23:19:56 +0100

    Ajout des fichiers calcul.py et test_operations.py

---
+++ calcul.py
@@ -0,0 +1,9 @@
+
+def addition(a, b):
+    return a + b
+
+def substraction(a, b):
+    return a - b
+
+def multiplication(a, b):
+    return a * b
---
+++ test_operations.py
@@ -0,0 +1,11 @@
+from calcul import addition, substraction, multiplication
+
+def assert_add():
+    assert addition(2, 3)==5
+
+def assert_sub():
+    assert substraction(4, 3)==1
+
+def assert_mult():
+    assert multiplication(2, 3)==6
+    
